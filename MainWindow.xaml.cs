211ef7bcf0761a7146f1bed8183c20d0fac035be
Dmitriy <shvetsovdv92@gmail.com>
2023-06-05 23:23:33 +0300

    add new project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+
+namespace Contracts
+{
+    public partial class MainWindow : Window
+    {
+        private Dictionary<string, string> signMappings = new Dictionary<string, string>()
+        {
+            { "Швецов Д.В.", "Швецова Дмитрия Вадимовича" },
+            { "Швецова С.В.", "Швецовой Светланы Вадимовны" },
+            { "Абрамова Е.Г.", "Абрамовой Елены Геннадьевны" },
+            { "Косыгина Р.В.", "Косыгина Романа Васильевича" }
+        };
+
+        private Dictionary<string, string> authorityMappings = new Dictionary<string, string>()
+        {
+            { "613", "доверенности №613 от \"18\" октября 2022г." },
+            { "824", "доверенности №824 от \"18\" декабря 2021г." },
+            { "488", "доверенности №488 от \"29\" августа 2022г." },
+            { "707", "доверенности №707 от \"29\" июля 2022г." }
+        };
+
+        private Dictionary<string, string> cityMappings = new Dictionary<string, string>()
+        {
+            { "МСК", "Москва" },
+            { "САМ", "Самара" },
+            { "НН", "Нижний Новгород" },
+            { "СПБ", "Санкт-Петербург" },
+            { "НСК", "Новосибирск" }
+        };
+
+        public string? Sign { get; private set; }
+        public string? City { get; private set; }
+        public string? Authority { get; private set; }
+        public long Price { get; private set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBoxSelectedSign(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            Sign = GetSignBySelectedItem(selectedItem);
+        }
+
+        private string GetSignBySelectedItem(ComboBoxItem selectedItem)
+        {
+            string selectedItemContent = selectedItem?.Content?.ToString() ?? string.Empty;
+
+            if (signMappings.ContainsKey(selectedItemContent))
+            {
+                return signMappings[selectedItemContent];
+            }
+
+            return string.Empty;
+        }
+
+        private void ComboBoxSelectedAuthority(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox1 = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox1.SelectedItem;
+            Authority = GetAuthorityBySelectedItem(selectedItem);
+        }
+
+        private string GetAuthorityBySelectedItem(ComboBoxItem selectedItem)
+        {
+            string selectedItemContent = selectedItem?.Content?.ToString() ?? string.Empty;
+
+            if (authorityMappings.ContainsKey(selectedItemContent))
+            {
+                return authorityMappings[selectedItemContent];
+            }
+
+            return string.Empty;
+        }
+
+        private void ComboBoxSelectedCity(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox1 = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox1.SelectedItem;
+            
+            City = GetCityBySelectedItem(selectedItem);
+        }
+
+        private string GetCityBySelectedItem(ComboBoxItem selectedItem)
+        {
+            string selectedItemContent = selectedItem?.Content?.ToString() ?? string.Empty;
+
+            if (cityMappings.ContainsKey(selectedItemContent))
+            {
+                return cityMappings[selectedItemContent];
+            }
+
+            return string.Empty;
+        }
+        private void TextBoxPrice(object sender, TextChangedEventArgs e)
+        {
+            TextBox priceTextBox = (TextBox)sender;
+            if (long.TryParse(priceTextBox.Text, out long priceValue))
+            {
+                Price = priceValue;
+            }
+           
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Open dialog window
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Word Documents|*.docx";
+            openFileDialog.Title = "Выберите файл шаблона";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Path to template
+                string templateFilePath = openFileDialog.FileName;
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ru-RU");
+
+                // Receiving data from the user
+                string fontName = "Arial";
+                float fontSize = 10;
+                string colorFont = "Black";
+
+                // Open dialog window
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Word Documents|*.docx";
+                saveFileDialog.Title = "Выберите путь для сохранения файла";
+                saveFileDialog.FileName = "Мой документ.docx";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    // Path to save the file
+                    string outputFilePath = saveFileDialog.FileName;
+
+                    // Open template
+                    using (var doc = DocX.Load(templateFilePath))
+                    {
+                        var font = new Xceed.Document.NET.Font(fontName);
+                        var fontColor = Color.FromName(colorFont);
+                        MyConverter myConverter = new MyConverter();
+                        string amountInWords = MyConverter.ConvertNumberToWords((long)Price);
+                        // Replace values
+                        ReplaceText(doc, "{date}", DateTime.Now.ToString("dd MMMM yyyy"), font, fontSize, fontColor);
+                        ReplaceText(doc, "{sign}", Sign, font, fontSize, fontColor);
+                        ReplaceText(doc, "{authority}", Authority, font, fontSize, fontColor);
+                        ReplaceText(doc, "{city}", City, font, fontSize, fontColor);
+                        ReplaceText(doc, "{Price}", Price + " (" + amountInWords + ")", font, fontSize, fontColor);
+                        // TODO: need add converter from number to words
+                       
+
+                        // Save new file
+                        doc.SaveAs(outputFilePath);
+                    }
+
+                    // Open updated contract
+                    Process.Start(new ProcessStartInfo(outputFilePath) { UseShellExecute = true });
+
+                    MessageBox.Show("Документ успешно создан и сохранен.");
+                }
+            }
+        }
+
+        private void ReplaceText(DocX document, string searchValue, string? newValue, Xceed.Document.NET.Font font, float fontSize, Color fontColor)
+        {
+            if (!string.IsNullOrEmpty(newValue))
+            {
+                var replaceTextOptions = new StringReplaceTextOptions()
+                {
+                    SearchValue = searchValue,
+                    NewValue = newValue,
+                    NewFormatting = new Formatting { FontFamily = font, Size = fontSize, FontColor = fontColor }
+                };
+                document.ReplaceText(replaceTextOptions);
+            }
+        }
+
+        private void ClearFields_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear fields
+            SignComboBox.SelectedIndex = -1; 
+            AtterneyComboBox.SelectedIndex = -1; 
+            CityComboBox.SelectedIndex = -1;
+            
+            ValuePrice.Text = string.Empty; 
+                                
+            Sign = null;
+            City = null;
+            Authority = null;
+            
+         
+        }
+    }
+}
---
+++ MyConverter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Contracts
+{
+    public class MyConverter
+    {
+        public static string ConvertNumberToWords(long price)
+        {
+            if (price == 0L)
+            {
+                return "ноль";
+            }
+
+            if (price < 0)
+            {
+                return "минус " + ConvertNumberToWords(Math.Abs(price));
+            }
+
+            string text = "";
+            if (price / 1000000000 > 0)
+                if (price >= 2000000000)
+                {
+                    text += ConvertNumberToWords(price / 1000000000) + " миллиарда ";
+                    price %= 1000000000;
+                }   
+                else
+                {
+                text += ConvertNumberToWords(price / 1000000000) + " миллиард ";
+                price %= 1000000000;
+                }
+            
+            if (price / 1000000 > 0)
+                if(price >= 2000000)
+                {
+                    text += ConvertNumberToWords(price / 1000000) + " миллиона ";
+                    price %= 1000000;
+                }
+                else
+                {
+                text += ConvertNumberToWords(price / 1000000) + " миллион ";
+                price %= 1000000;
+                }
+
+            if (price / 1000 > 0)
+                if (price >= 5000)
+                {
+                    text += ConvertNumberToWords(price / 1000) + " тысяч ";
+                    price %= 1000;
+                }
+                else if (price>=2000)
+                {
+                    text += ConvertNumberToWords(price / 1000) + " тысячи ";
+                    price %= 1000;
+                }
+                else
+                {
+                text += ConvertNumberToWords(price / 1000) + " тысяча ";
+                price %= 1000;
+                }
+
+
+            if (price > 0)
+            {
+                string[] array = new string[20] { "ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
+                string[] array2 = new string[10] { "ноль", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" };
+                string[] array3 = new string[10] { "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот", "тысяча" };
+
+                if (price >= 100)
+                {
+                    text += array3[price / 100 - 1] + " ";
+                    price %= 100;
+                }
+
+                if (price < 20)
+                {
+                    text += array[price];
+                }
+                else
+                {
+                    text += array2[price / 10];
+                    if (price % 10 > 0)
+                    {
+                        text += " " + array[price % 10];
+                    }
+                }
+            }
+
+            return text;
+        }
+    }
+}
+
